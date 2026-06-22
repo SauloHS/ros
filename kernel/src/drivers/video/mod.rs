1bb7429b65c62170b1e38db6ac11b573bc1e8b9e
Saulo Henrique Santos Dorotéio <doroteios@outlook.com>
2026-06-22 14:44:13 -0300

    Git hooks update

--- kernel/src/drivers/video/mod.rs
+++ kernel/src/drivers/video/mod.rs
@@ -3,9 +3,4 @@ File created by Saulo Henrique Santos Dorotéio.
 Last updated by Saulo Henrique Santos Dorotéio, at 06/22/2026.
 See LICENSE file for licensing information */
 
-/*
-File created by Saulo Henrique Santos Dorotéio.
-Last updated by Saulo Henrique Santos Dorotéio, at 06/22/2026.
-See LICENSE file for licensing information */
-
 pub mod framebuffer; 
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,11 +3,6 @@ File created by Saulo Henrique Santos Dorotéio.
 Last updated by Saulo Henrique Santos Dorotéio, at 06/22/2026.
 See LICENSE file for licensing information */
 
-/*
-File created by Saulo Henrique Santos Dorotéio.
-Last updated by Saulo Henrique Santos Dorotéio, at 06/22/2026.
-See LICENSE file for licensing information */
-
 #![no_std]
 #![no_main]
 
